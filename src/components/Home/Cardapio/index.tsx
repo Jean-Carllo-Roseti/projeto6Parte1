eb34a4556dee090fd27bb625b8dca7241235754f
jean carllo <jeanrosete04@gmail.com>
2024-03-20 15:41:21 -0300

    tentiva de normalizar perfil.

--- src/components/Home/Cardapio/index.tsx
+++ src/components/Home/Cardapio/index.tsx
@@ -1,4 +1,4 @@
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
 import {
   CardCadapio,
@@ -13,34 +13,56 @@ import {
 
 import Estrela from '../../../assets/images/estrela.png'
 import { Pratos } from '../../../Pages/Home'
+import { useEffect, useState } from 'react'
 
 type Props = Pick<
   Pratos,
   'titulo' | 'descricao' | 'avaliacao' | 'capa' | 'tipo'
->
+> & {
+  cardapio?: { id: number }
+}
 
-const Cardapio = ({ tipo, titulo, descricao, avaliacao, capa }: Props) => (
-  <>
-    <CardCadapio>
-      <ImgCardapio capa={capa}>
-        <Etiqueta>{tipo}</Etiqueta>
-      </ImgCardapio>
-      <Borda>
-        <CadapiorListaTag>
-          <li>
-            <h3>{titulo}</h3>
-          </li>
-          <li>
-            <Nota>
-              {avaliacao} <ImgEstrela src={Estrela} />
-            </Nota>
-          </li>
-        </CadapiorListaTag>
-        <TextCardapio>{descricao}</TextCardapio>
-        <Link to="/perfil">Saiba mais</Link>
-      </Borda>
-    </CardCadapio>
-  </>
-)
+const Cardapio = ({
+  tipo,
+  titulo,
+  descricao,
+  avaliacao,
+  cardapio,
+  capa
+}: Props) => {
+  const { id } = useParams()
+
+  const [prato, setPratos] = useState<Pratos[]>()
+
+  useEffect(() => {
+    fetch(`https://fake-api-tau.vercel.app/api/efood/restaurantes${id}`)
+      .then((res) => res.json())
+      .then((res) => setPratos(res))
+  }, [id])
+
+  return (
+    <>
+      <CardCadapio>
+        <ImgCardapio capa={capa}>
+          <Etiqueta>{tipo}</Etiqueta>
+        </ImgCardapio>
+        <Borda>
+          <CadapiorListaTag>
+            <li>
+              <h3>{titulo}</h3>
+            </li>
+            <li>
+              <Nota>
+                {avaliacao} <ImgEstrela src={Estrela} />
+              </Nota>
+            </li>
+          </CadapiorListaTag>
+          <TextCardapio>{descricao}</TextCardapio>
+          <Link to={`perfil/${cardapio?.id ?? 'default'}`}>Saiba mais</Link>
+        </Borda>
+      </CardCadapio>
+    </>
+  )
+}
 
 export default Cardapio
