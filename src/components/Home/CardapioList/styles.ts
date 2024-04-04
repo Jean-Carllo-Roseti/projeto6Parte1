aa2bf639d41a8546844b79d535c46a9fecafd5e9
jean carllo <jeanrosete04@gmail.com>
2024-04-04 14:44:13 -0300

    ajuste de layout.

--- src/components/Home/CardapioList/styles.ts
+++ src/components/Home/CardapioList/styles.ts
@@ -8,8 +8,13 @@ export const SectionList = styled.section`
   width: 1024px;
   max-width: 100%;
   margin: 0 auto;
-  margin-top: 15%;
-  margin-bottom: 15%;
+  margin-top: 80px;
+  margin-bottom: 120px;
+
+  @media (max-width: ${breakpoints.tablet}) {
+    margin-top: 15%;
+    margin-bottom: 15%;
+  }
 `
 
 export const List = styled.ul`
